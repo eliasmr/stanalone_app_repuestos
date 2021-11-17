1134d6e0151a6dc76dd6d51a28b96cdf03b8678f
Cesar Martinez <cesarmartinez@cbit-online.com>
2021-11-17 15:16:29 -0500

    esqueleto

---
+++ Sistema-De-Inventarios-master/src/sistemaDeInventarios/SistemaDeInventarios.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistemaDeInventarios;
+
+
+import Vista.FrmPrincipal;
+
+/**
+ *
+ * @author ialonzo
+ */
+public class SistemaDeInventarios {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmPrincipal frm = new FrmPrincipal();
+        frm.setVisible(true);
+    }
+    
+}
