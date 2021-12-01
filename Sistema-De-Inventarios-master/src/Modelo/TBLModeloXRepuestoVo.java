0fffe49f4f75d432ca72ede61b2c40853c5187e0
emedina <elias.medina@fuac.edu.co>
2021-11-30 20:51:58 -0500

    ajustes

---
+++ Sistema-De-Inventarios-master/src/Modelo/TBLModeloXRepuestoVo.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author Elias
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TBLModeloXRepuestoVo {
+    private int id;
+    private TBLModeloVo modelo;
+    private TBLRepuestoVo repuesto;
+    
+}
